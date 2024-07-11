0d65b49baac8b43b085bb1910cd70dd28e3d3c0c
ghalib_24 <muhamadghalib7@gmail.com>
2024-07-11 17:57:26 +0700

    feat(dto): add paginated result dto

---
+++ use-case/src/common/dto/paginated-result.dto.ts
@@ -0,0 +1,23 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class PaginatedResultDto<T> {
+  data: T[];
+
+  @ApiProperty({
+    description: "Current page number",
+    example: 1,
+  })
+  page: number;
+
+  @ApiProperty({
+    description: "Number of entries per page",
+    example: 10,
+  })
+  entries: number;
+
+  @ApiProperty({
+    description: "Total number of items available",
+    example: 1,
+  })
+  total_count: number;
+}
