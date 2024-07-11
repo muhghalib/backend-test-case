ddd2c6a5ee34607062d2c25d2d4abbc8b66a0fa6
ghalib_24 <muhamadghalib7@gmail.com>
2024-07-11 17:57:40 +0700

    feat(dto): add pagination dto

---
+++ use-case/src/common/dto/pagination.dto.ts
@@ -0,0 +1,27 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional } from "class-validator";
+
+export class PaginationDto {
+  @ApiProperty({
+    description: "The number of records per page",
+    example: 1,
+    default: 1,
+    required: false,
+  })
+  @Type(() => Number)
+  @IsOptional()
+  @IsNumber()
+  page?: number = 1;
+
+  @ApiProperty({
+    description: "The number of records per page",
+    example: 10,
+    default: 10,
+    required: false,
+  })
+  @Type(() => Number)
+  @IsOptional()
+  @IsNumber()
+  limit?: number = 1;
+}
